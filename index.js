828327d4e4e522ce29e5bc0a477219a3213cdc87
fabianonunes <fabiano@nunes.me>
2016-11-24 17:49:01 -0200

    wip

---
+++ index.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+const Config = require('webpack-config').Config
+const path = require('path')
+
+module.exports = function (config) {
+  const conf = new Config().extend(
+    path.join(__dirname, 'config.js')
+  ).merge(config)
+
+  if (config.entry.vendor) {
+    conf.merge({
+      plugins: [
+        // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.[hash].js')
+        new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor',
+          minChunks: Infinity,
+          filename: 'vendor.[hash].js'
+        })
+      ]
+    })
+  }
+
+  return conf
+}
