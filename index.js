31d5e7fbecc4337ce55b754b7d1877d8431b1d40
fabianonunes <fabiano@nunes.me>
2016-11-25 20:04:39 -0200

    remover commons do index

--- index.js
+++ index.js
@@ -4,21 +4,7 @@ const path = require('path')
 const dlv = require('dlv')
 
 module.exports = function (config) {
-  const conf = new Config().extend(
+  return new Config().extend(
     path.join(__dirname, 'config.js')
   ).merge(config)
-
-  if (dlv(config, 'entry.vendors')) {
-    conf.merge({
-      plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-          name: 'vendors',
-          minChunks: Infinity,
-          filename: 'js/vendors.[hash].js'
-        })
-      ]
-    })
-  }
-
-  return conf
 }
